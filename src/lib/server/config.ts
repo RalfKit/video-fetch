0692d412f03b73bbf1a0fe4f9da509168db51fe4
Ralf <66371975+RalfKit@users.noreply.github.com>
2026-05-10 12:23:17 +0200

    feat(config): add temporary download folder

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -7,10 +7,21 @@ export const DOWNLOAD_FOLDER = private_env.DOWNLOAD_PATH
 	? path.resolve(private_env.DOWNLOAD_PATH)
 	: path.resolve('./downloads');
 
+export const TEMP_DOWNLOAD_FOLDER = private_env.TEMP_DOWNLOAD_PATH
+	? path.resolve(private_env.TEMP_DOWNLOAD_PATH)
+	: path.resolve(DOWNLOAD_FOLDER, '.incomplete');
+
 // Stelle sicher, dass der Ordner existiert
 if (!fs.existsSync(DOWNLOAD_FOLDER)) {
 	fs.mkdirSync(DOWNLOAD_FOLDER, { recursive: true });
 	console.log(`Download folder created: ${DOWNLOAD_FOLDER}`);
 } else {
 	console.log(`Download folder exists: ${DOWNLOAD_FOLDER}`);
+}
+
+if (!fs.existsSync(TEMP_DOWNLOAD_FOLDER)) {
+	fs.mkdirSync(TEMP_DOWNLOAD_FOLDER, { recursive: true });
+	console.log(`Temporary download folder created: ${TEMP_DOWNLOAD_FOLDER}`);
+} else {
+	console.log(`Temporary download folder exists: ${TEMP_DOWNLOAD_FOLDER}`);
 }
