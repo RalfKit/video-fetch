42367b1a87ae1291a25e173aa0d07da36e71c098
Ralf <66371975+RalfKit@users.noreply.github.com>
2026-05-10 12:25:34 +0200

    feat(svelte): enable experimental async

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
 import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter(), csrf: { trustedOrigins: ['*'] } } };
+const config = {
+	kit: {
+		adapter: adapter(),
+		csrf: { trustedOrigins: ['*'] },
+		experimental: { remoteFunctions: true }
+	},
+	compilerOptions: { experimental: { async: true } }
+};
 
 export default config;
